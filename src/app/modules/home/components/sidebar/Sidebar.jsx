c4e5cd8804b158ee58448e2e01a14818bb7dd07c
tripalidex <carlosquispe1805@gmail.com>
2024-06-24 10:51:09 -0500

    Implementacion de login y rutas protegidas segun los roles

--- src/app/modules/home/components/sidebar/Sidebar.jsx
+++ src/app/modules/home/components/sidebar/Sidebar.jsx
@@ -4,14 +4,27 @@ import logoUsil from '../../../../../assets/logo-usil.png'
 import { IoPricetags } from "react-icons/io5"
 import { BsArrowLeftShort } from "react-icons/bs"
 import { useState } from "react"
+import { FaUsers } from "react-icons/fa"
+import useAuth from "../../../../../hooks/useAuth"
+import { TbLogout2 } from "react-icons/tb"
+import { HiMiniShoppingBag } from "react-icons/hi2"
 
 const Sidebar = () => {
+  const { auth } = useAuth();
+
   const [open, setOpen] = useState(true);
   const Menus = [
+    { title: "Ventas", icon: <HiMiniShoppingBag />, to: "sales" },
     { title: "Libros", icon: <RiBookOpenFill />, to: "books" }, 
     { title: "Autores", icon: <RiBallPenFill />, to: "authors" }, 
-    { title: "Categorías", icon: <IoPricetags />, to: "categories" }, 
+    { title: "Editoriales", icon: <IoPricetags />, to: "editoriales" }, 
+    { title: "Empleados", icon: <FaUsers />, to: "employees", role: 'ADMIN' }
   ];
+
+  const filteredMenus = Menus.filter((menu) => {
+    // Si el menú tiene un rol permitido y el usuario no tiene ese rol, no lo muestra
+    return !menu.role || (auth && auth.rol === menu.role);
+  });
 
   return (
     <>
@@ -37,10 +50,9 @@ const Sidebar = () => {
             USIL
           </h1>
         </div>
-
         {/* MODULOS */}
         <ul className="pt-2">
-          {Menus.map((menu, index) => (
+          {filteredMenus.map((menu, index) => (
             <>
               <li>
                 <Link to={menu.to} key={index} className="text-white text-sm flex 
@@ -57,7 +69,18 @@ const Sidebar = () => {
               </li>
             </>
           ))}
-        </ul>        
+        </ul>
+        <Link to="/login" className="text-white text-sm flex 
+                items-center gap-x-4 cursor-pointer p-2
+                hover:bg-white hover:text-usil-color rounded-lg mt-2">
+                  <span className="text-2xl block float-left">
+                    <TbLogout2 />
+                  </span>
+                  <span className={`text-base font-medium flex-1 
+                    duration-200 ${!open && "hidden"}`}>
+                    Cerrar sesión
+                  </span>
+        </Link>
       </div>
     </>
     
